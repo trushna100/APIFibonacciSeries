4f74d4a96165278cfced4b72b9781c04c42e4285
trushna100 <52227802+trushna100@users.noreply.github.com>
2021-12-14 17:47:02 -0800

    First Commit

---
+++ FibAPI/FibAPI/Controllers/FibonacciController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FibAPI.Services;
+
+namespace FibAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+       
+        private readonly ILogger<FibonacciController> _logger;
+        private readonly FibonacciService _service;
+
+        public FibonacciController(ILogger<FibonacciController> logger, FibonacciService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        /// <summary>
+        /// API endpoint for getting Fibonacci Sequence
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<int> Get(int len)
+        {
+            return _service.GetSequence(len);
+        }
+    }
+}
---
+++ FibAPI/FibAPI/Services/FibonacciService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FibAPI.Services
+{
+    public class FibonacciService
+    {
+        public FibonacciService() { }
+
+        /// <summary>
+        /// Iterative approach to display the Fibonacci series
+        /// </summary>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public List<int> GetSequence(int len)
+        {
+            
+            if(len < 1)
+            {
+                return new List<int>();
+            }
+
+            int a = 0, b = 1;
+            if (len == 1)
+            {
+                return new List<int> { a };
+            }
+
+            
+            var result = new List<int>(len) { a, b };
+
+            if (len == 2) 
+            { 
+                return result;
+            }
+           
+            for (int i = 2; i < len; i++)
+            {
+                var c = a + b;
+                result.Add(c);
+                a = b;
+                b = c;
+            }
+
+            return result;
+        }
+    }
+}
